9ee07a9bf10509aab68f40d1b7398c9567e7b0c4
Isaac Thomas <thomasisaac245@utexas.edu>
2026-04-15 17:10:33 -0500

    fixed issue with submit button

--- frontend/src/components/ChatInput.jsx
+++ frontend/src/components/ChatInput.jsx
@@ -1,33 +1,33 @@
-import useAutosize from '@/hooks/useAutosize';
-import sendIcon from '@/assets/images/send.svg';
+import useAutosize from "@/hooks/useAutosize";
+import sendIcon from "@/assets/images/send.svg";
 
 function ChatInput({ newMessage, isLoading, setNewMessage, submitNewMessage }) {
   const textareaRef = useAutosize(newMessage);
 
   function handleKeyDown(e) {
-    if(e.keyCode === 13 && !e.shiftKey && !isLoading) {
+    if (e.keyCode === 13 && !e.shiftKey && !isLoading) {
       e.preventDefault();
       submitNewMessage();
     }
   }
-  
-  return(
-    <div className='sticky bottom-0 shrink-0 bg-black py-4'>
-      <div className='p-1.5 bg-primary-blue/20 rounded-3xl z-50 font-mono origin-bottom animate-chat duration-400'>
-        <div className='pr-0.5 bg-black relative shrink-0 rounded-3xl overflow-hidden ring-primary-blue/40 focus-within:ring-primary-blue ring-1 transition-all'>
+
+  return (
+    <div className="sticky bottom-0 shrink-0 bg-black py-4">
+      <div className="p-1.5 bg-primary-blue/20 rounded-3xl z-50 font-mono origin-bottom animate-chat duration-400">
+        <div className="pr-0.5 bg-black relative shrink-0 rounded-3xl overflow-hidden ring-primary-blue/40 focus-within:ring-primary-blue ring-1 transition-all">
           <textarea
-            className='block w-full max-h-[140px] py-2 px-4 pr-11 bg-black text-primary-blue rounded-3xl resize-none placeholder:text-white placeholder:leading-4 placeholder:-translate-y-1 sm:placeholder:leading-normal sm:placeholder:translate-y-0 focus:outline-hidden'
+            className="block w-full max-h-[140px] py-2 px-4 pr-11 bg-black text-primary-blue rounded-3xl resize-none placeholder:text-white placeholder:leading-4 placeholder:-translate-y-1 sm:placeholder:leading-normal sm:placeholder:translate-y-0 focus:outline-hidden"
             ref={textareaRef}
-            rows='1'
+            rows="1"
             value={newMessage}
-            onChange={e => setNewMessage(e.target.value)}
+            onChange={(e) => setNewMessage(e.target.value)}
             onKeyDown={handleKeyDown}
           />
           <button
-            className='absolute top-1/2 -translate-y-1/2 right-3 p-1 rounded-md hover:bg-primary-blue/20'
-            onClick={submitNewMessage}
+            className="absolute top-1/2 -translate-y-1/2 right-3 p-1 rounded-md hover:bg-primary-blue/20"
+            onClick={() => submitNewMessage()}
           >
-            <img src={sendIcon} alt='send' />
+            <img src={sendIcon} alt="send" />
           </button>
         </div>
       </div>
